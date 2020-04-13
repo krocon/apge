740f62192353154c5b6bf3bc4defdaff314791c4
Marc Kronberg <krocon@gmail.com>
2020-04-13 14:18:35 +0200

    v00.12

--- bin/apge.js
+++ bin/apge.js
@@ -1,29 +1,28 @@
 #!/usr/bin/env node
 
-const program = require('commander');
+const {Command} = require('commander');
 const chalk = require('chalk');
 const apge = require('../lib');
 const update = require('../lib/update.js');
-const version = require('../lib/version.js');
 const help = require('../lib/help.js');
+const pkg = require('../package.json');
 
+const program = new Command();
+
+program.version(pkg.version, '-v, --version', 'output the current version')
 
 program
-  // .arguments('<file>')
-  .option('-v, --version', 'Prints the version number of apge')
   .option('-h, --help', 'Prints the version number of apge')
   .option('-u, --update', 'Update of npm, angular cli and apge')
   .option('-p, --prefix <prefix>', 'The app prefix (default: db)', 'db')
-  .option('-a, --app <appname>', 'The app name (default: db-demo)', 'db-demo')
+  .option('-a, --app <appname>', 'The app name (default: db-demo)', 'db-demo');
+
+program
   .action(() => {
       // console.info(JSON.stringify(program, null, 4)); // TODO weg
       if (program.update) {
         // user entered:  apge --update
         update();
-
-      } else if (program.version) {
-        // user entered:  apge --version
-        version();
 
       } else if (program.help) {
         // user entered:  apge --help
@@ -40,5 +39,7 @@ program
       }
       process.exit(1);
     }
-  )
+  );
+
+program
   .parse(process.argv);
