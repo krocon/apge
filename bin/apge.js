51ff1f6f20551c888697457e442ee21a37b67af6
Marc Kronberg <krocon@gmail.com>
2020-04-25 17:51:14 +0200

    generate cmd added

--- bin/apge.js
+++ bin/apge.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 const {Command} = require('commander');
+const fs = require('fs-extra');
 const apge = require('../lib/cmd-generate-new.js');
 const update = require('../lib/cmd-update.js');
 const pkg = require('../package.json');
 const chalk = require('chalk');
-const {generateStoreservice} = require("../lib/cmd-generate-storeservice");
+const {generateStoreService} = require("../lib/cmd-generate-storeservice");
+const {generateRestService} = require("../lib/cmd-generate-restservice");
 const {printFiglet} = require("../lib/print-figlet");
 
 const program = new Command();
@@ -62,36 +64,40 @@ program
   .description('Generate a new schematic [restservice, storeservice]', 'storeservice')
   .option('-f, --force', 'No confirm dialog / no prompts.', false)
   .action(async (p1, p2) => {
-    if (p1 && p2) {
-      const force = p1.force === 'true' || p1.force === '1';
-      const schematic = p2[0];
-      const name = p2[1];
-
-      console.info('force', JSON.stringify(force));
-      console.info('schematic', JSON.stringify(schematic));
-      console.info('name', JSON.stringify(name));
-
-      if (!name) {
-        console.error(chalk.bold.red('Name is missing.'));
-        console.error(chalk.bold.red('Try:  apge generate --help'));
-
-      } else if (schematic === 'storeservice') {
-        await generateStoreservice(name);
-
-      } else if (schematic === 'restservice') {
-        // todo generateStoreservice(name);
-        console.info('Not implemented yet');
-
-      } else {
-        console.error(chalk.bold.red('The generate command requires a type [restservice, storeservice] and a name.'));
-        console.error(chalk.bold.red('Try:  apge generate --help'));
+      let jsonObject;
+      try {
+        jsonObject = fs.readJsonSync('apge.json');
+      } catch (e) {
+        console.error(chalk.bold.red('The generate command requires to be run in an Angular project, but a project definition (apge.json) could not be found.'));
+        console.error(e);
+        process.exit(-1);
       }
 
-    } else {
-      console.error(chalk.bold.red('Try:  apge generate --help'));
-    }
+      if (p1 && p2 && p2.length > 1) {
+        console.info('_force', JSON.stringify(p1.force));
 
+        const force = p1.force === 'true' || p1.force === '1';
+        const schematic = p2[0];
+        const name = p2[1];
 
+        if (!name) {
+          console.error(chalk.bold.red('Name is missing.'));
+          console.error(chalk.bold.red('Try:  apge generate --help'));
+
+        } else if (schematic === 'storeservice') {
+          await generateStoreService(name);
+
+        } else if (schematic === 'restservice') {
+          await generateRestService(name);
+
+        } else {
+          console.error(chalk.bold.red('The generate command requires a type [restservice, storeservice] and a name.'));
+          console.error(chalk.bold.red('Try:  apge generate --help'));
+        }
+
+      } else {
+        console.error(chalk.bold.red('Try:  apge generate --help'));
+      }
     }
   )
   .on('--help', () => {
